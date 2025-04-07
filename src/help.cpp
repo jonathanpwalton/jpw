8f101b176ba0bab79f5c0c57787e726e530fdb68
Jonathan Walton <jonathan.walton@snhu.edu>
2025-04-07 13:33:14 +0900

    use raw string in help output

--- src/help.cpp
+++ src/help.cpp
@@ -4,12 +4,12 @@ void cmd::help() {
   if (!argv.empty())
     throw error("the 'help' command does not take any arguments");
 
-  std::cout <<
-    "usage: jpw <command>\n" <<
-    "\n" <<
-    "commands:\n" <<
-    "  pull [<package> [-p <provider>] [-v <version>] ...]     update local repository and install packages\n" <<
-    "  drop <package> ...                                     uninstall and remove packages\n" <<
-    "  show {available | installed}                           show all available or installed packages\n" <<
-    "  help                                                   display this help text and quit\n";
+  std::cout << R"(usage: jpw <command>
+
+commands:
+  pull [<package> [-p <provider>] [-v <version>] ...]    update local repository and install packages
+  drop <package> ...                                     uninstall and remove packages
+  show {available | installed}                           show all available or installed packages
+  help                                                   display this help text and quit
+)" << std::flush;
 }
